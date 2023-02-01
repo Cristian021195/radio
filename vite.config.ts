0edde968260d51d37daececaaf1fc49942d8292a
cristian021195 <cristiangramajo015@gmail.com>
2023-02-01 01:14:50 -0300

    segundo commit

--- vite.config.ts
+++ vite.config.ts
@@ -9,9 +9,6 @@ import { VitePWA } from 'vite-plugin-pwa'
 */
 export default defineConfig({
   plugins: [react(), VitePWA({ 
-    registerType: 'autoUpdate',
-    devOptions: {
-      enabled: true
-    }
+    registerType: 'autoUpdate'
   })]
 })
